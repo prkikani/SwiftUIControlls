08565053cf2a30ae7bcfd17d35bd03500099a65a
PRADIP KIKANI <prkikani@gmail.com>
2021-09-07 20:28:06 +0530

    uploaded First Target

---
+++ SwiftUIControlls/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by pradip R. kikani on 07/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIControlls/Shared/SwiftUIControllsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIControllsApp.swift
+//  Shared
+//
+//  Created by pradip R. kikani on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIControllsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
